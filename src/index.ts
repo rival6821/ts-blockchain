4c91144352553db7c73f07325817129a5e3f8258
ilhoon <rival6821@naver.com>
2021-01-01 23:19:41 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const name = "ilhoon",
   gender = "male";
 
 const sayHi = (name: string, age: number, gender: string): void => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}!`);
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}!!`);
 }
 
 sayHi(name, age, gender);
