d682817c0d0ee6ebd35676d4a640360da50da333
ilhoon <rival6821@naver.com>
2021-01-02 13:41:18 +0900

    CryptoJS setting and some functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (index: number,
+    previousHash: string,
+    data: string,
+    timestamp: number): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
   constructor(index: number,
     hash: string,
@@ -21,6 +28,12 @@ class Block {
 const genesisBlock: Block = new Block(0, '7349853724589', '', 'hello', 123456);
 
 let blockchain: Array<Block> = [genesisBlock];
+
+const getBlockChain = (): Array<Block> => blockchain;
+
+const getLatestBlockChain = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockchain);
 
