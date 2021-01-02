61754fc11344d8ddec12b9ffdbf32cd7cce26ca3
ilhoon <rival6821@naver.com>
2021-01-02 13:05:35 +0900

    create block

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+  constructor(index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const me = new Human('ilhoon', 34, 'male');
+const genesisBlock: Block = new Block(0, '7349853724589', '', 'hello', 123456);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`;
-}
+let blockchain: Array<Block> = [genesisBlock];
 
-console.log(sayHi(me));
+console.log(blockchain);
 
 export { };
